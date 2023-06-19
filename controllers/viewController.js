27c03f456022459800d457622cbb8e416ecfb61a
Joshua Trujillo <joshmtrujillo@gmail.com>
2023-06-19 00:45:21 -0400

    path change

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -24,7 +24,7 @@ exports.tenants = async (req, res) => {
   const endpoint = process.env.RENTEC_LINK;
   const rentec = await axios.post(endpoint);
   const listings = rentec.data.properties;
-  console.log(rentec.data.properties);
+  // console.log(rentec.data.properties);
   res.status(200).render("tenants", {
     listings,
   });
