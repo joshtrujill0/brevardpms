39bb1e9015ab5dee49ae5bbfed2e12e681e25782
Joshua Trujillo <joshmtrujillo@gmail.com>
2023-06-20 16:48:41 -0400

    mobile menu added

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -3,3 +3,18 @@ document.documentElement.style.setProperty(
   "--scroll-padding",
   navHeight - 10 + "px"
 );
+
+const primaryNav = document.querySelector(".menu__primary-nav");
+const navToggle = document.querySelector(".mobile-toggle");
+
+navToggle.addEventListener("click", () => {
+  const state = primaryNav.getAttribute("data-visible");
+
+  state === "false"
+    ? primaryNav.setAttribute("data-visible", true)
+    : primaryNav.setAttribute("data-visible", false);
+
+  state === "false"
+    ? primaryNav.setAttribute("aria-expanded", true)
+    : primaryNav.setAttribute("aria-expanded", false);
+});
