558e2e59f761fa7db71c4b89eadd172c55490f78
Joshua Trujillo <joshmtrujillo@gmail.com>
2024-01-15 14:28:55 -0500

    adding agent bios

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -5,3 +5,15 @@ faqs.forEach((faq) => {
     faq.classList.toggle("active");
   });
 });
+
+window.addEventListener("click", (e) => {
+  const pageElement = e.target;
+  if (pageElement.tagName == "A") {
+    const value = pageElement.href;
+    console.log(value);
+  }
+  if (pageElement.tagName == "INPUT") {
+    const value = pageElement.type;
+    console.log(value);
+  }
+});
